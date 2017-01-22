d602a227079af88051484bc40da6bb2bef5c1c08
tasnia kamal <tasniakamal@foos-MacBook-Pro.local>
2017-01-22 23:38:21 +0000

    created bank

--- bank.py
+++ bank.py
@@ -2,5 +2,20 @@ class Account():
 	def __init__(self, name):
 		self.name = name
 		self.balance = 0.0
+
 	def apply_interest(self, interest):
 		self.balance *= interest
+
+class Bank():
+	def __init__(self):
+		self.accounts = []
+
+	def create_account(self, name):
+		account = Account(name)
+		self.accounts.append(account)
+
+if __name__ == "__main__":
+	import pdb
+	pdb.set_trace()
+	bank = Bank()
+	bank.create_account("Tasnia")
