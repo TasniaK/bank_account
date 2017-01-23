f5e3a93b676c29fd6647ac452305d9a42b00d8bc
tasnia kamal <tasniakamal@foos-MacBook-Pro.local>
2017-01-23 16:48:46 +0000

    comments added

--- bank.py
+++ bank.py
@@ -2,12 +2,15 @@
 from datetime import datetime, timedelta
 import time
 
-
+#classes used as blueprints for functions
+#external "main" code runs class Bank, class Bank runs class Account.
 class Account():
 	def __init__(self, name):
+		#self. allows you to attach variables to the account being created
 		self.name = name
 		self.balance = 0.0
 		self.account_created = datetime.now()
+		#time stamp for each account created
 
 	def apply_interest(self, interest):
 		self.balance *= interest
@@ -15,18 +18,24 @@ class Account():
 
 class Bank():
 	def __init__(self):
+		#accounts created in Account stored as list in Bank
 		self.accounts = []
 		self.interest = 1.1
+		#set interval at which interest is applied to per minute
 		self.interest_interval = timedelta(minutes=1)
+	
 
 	def create_account(self, name):
 		account = Account(name)
 		self.accounts.append(account)
 
 	def apply_interest(self):
+		#conditions to apply interest: loop through accounts list in Bank, time now is greater than time past
+		#check against set interval
 		for account in self.accounts:
 			if datetime.now() - account.account_created > self.interest_interval:
 				account.apply_interest(self.interest)
+		
 
 if __name__ == "__main__":
 	#import pdb
